daacb8cb8da991e1a0905829679de7b4a2dc55a7
Mateusz Boczarski <matbocz@outlook.com>
2019-09-03 16:21:56 +0200

    Update interface

--- main.py
+++ main.py
@@ -12,60 +12,61 @@ class InterpolationComparison(QWidget):
         self.resize(600, 1)
 
         # Layout
-        layout = QGridLayout()
+        self.layout = QGridLayout()
 
         # Labels
-        lbl1 = QLabel("Enter starting value:")
-        lbl2 = QLabel("Enter end value:")
-        lbl3 = QLabel("Enter number of samples:")
-        lbl4 = QLabel("Enter interpolated function (example: sin(x) + 2):")
-        lbl5 = QLabel("Choose first kind of interpolation:")
-        lbl6 = QLabel("Choose second kind of interpolation:")
+        self.lbl1 = QLabel("Enter starting value:")
+        self.lbl2 = QLabel("Enter end value:")
+        self.lbl3 = QLabel("Enter number of samples:")
+        self.lbl4 = QLabel(
+            "Enter interpolated function (example: sin(x) + 2):")
+        self.lbl5 = QLabel("Choose first kind of interpolation:")
+        self.lbl6 = QLabel("Choose second kind of interpolation:")
 
         # LineEdits
-        edit1 = QLineEdit()
-        edit2 = QLineEdit()
-        edit3 = QLineEdit()
-        edit4 = QLineEdit()
+        self.edit1 = QLineEdit()
+        self.edit2 = QLineEdit()
+        self.edit3 = QLineEdit()
+        self.edit4 = QLineEdit()
 
         # ComboBoxs
-        box1 = QComboBox()
-        box1.addItems([
+        self.box1 = QComboBox()
+        self.box1.addItems([
             "linear", "nearest", "zero", "slinear", "quadratic", "cubic",
             "previous", "next"
         ])
-        box2 = QComboBox()
-        box2.addItems([
+        self.box2 = QComboBox()
+        self.box2.addItems([
             "linear", "nearest", "zero", "slinear", "quadratic", "cubic",
             "previous", "next"
         ])
 
         # Buttons
-        btn1 = QPushButton("START!")
+        self.btn1 = QPushButton("START!")
 
         # Labels to layout
-        layout.addWidget(lbl1, 0, 0)
-        layout.addWidget(lbl2, 1, 0)
-        layout.addWidget(lbl3, 2, 0)
-        layout.addWidget(lbl4, 3, 0)
-        layout.addWidget(lbl5, 4, 0)
-        layout.addWidget(lbl6, 5, 0)
+        self.layout.addWidget(self.lbl1, 0, 0)
+        self.layout.addWidget(self.lbl2, 1, 0)
+        self.layout.addWidget(self.lbl3, 2, 0)
+        self.layout.addWidget(self.lbl4, 3, 0)
+        self.layout.addWidget(self.lbl5, 4, 0)
+        self.layout.addWidget(self.lbl6, 5, 0)
 
         # LineEdits to layout
-        layout.addWidget(edit1, 0, 1)
-        layout.addWidget(edit2, 1, 1)
-        layout.addWidget(edit3, 2, 1)
-        layout.addWidget(edit4, 3, 1)
+        self.layout.addWidget(self.edit1, 0, 1)
+        self.layout.addWidget(self.edit2, 1, 1)
+        self.layout.addWidget(self.edit3, 2, 1)
+        self.layout.addWidget(self.edit4, 3, 1)
 
         # ComboBoxs to layout
-        layout.addWidget(box1, 4, 1)
-        layout.addWidget(box2, 5, 1)
+        self.layout.addWidget(self.box1, 4, 1)
+        self.layout.addWidget(self.box2, 5, 1)
 
         # Buttons to layout
-        layout.addWidget(btn1, 6, 1)
+        self.layout.addWidget(self.btn1, 6, 1)
 
         # Set layout
-        self.setLayout(layout)
+        self.setLayout(self.layout)
         self.show()
 
 
