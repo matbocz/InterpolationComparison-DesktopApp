efdab3c9360f3b751ec71c4c1b5a9fd712dd2b06
Mateusz Boczarski <matbocz@outlook.com>
2019-09-09 20:52:51 +0200

    Minor improvements

--- main.py
+++ main.py
@@ -9,22 +9,13 @@ from PyQt5.QtCore import Qt
 import interpolation
 
 
-class InterpolationComparison(QWidget):
-    """This is a class for interpolation comparison."""
+class ApplicationGUI(QWidget):
+    """This is a class that creates a graphical user interface."""
     def __init__(self):
         super().__init__()
-        self.interface()
 
-    def interface(self):
-        """This is a function to create GUI."""
-
-        self.setWindowTitle("Interpolation Comparison")
-        self.resize(600, 1)
-
-        # Layout
         self.layout = QGridLayout()
 
-        # Labels
         self.lbl1 = QLabel("Enter starting value:")
         self.lbl2 = QLabel("Enter end value:")
         self.lbl3 = QLabel("Enter number of samples:")
@@ -33,13 +24,11 @@ class InterpolationComparison(QWidget):
         self.lbl5 = QLabel("Choose first kind of interpolation:")
         self.lbl6 = QLabel("Choose second kind of interpolation:")
 
-        # LineEdits
         self.edit1 = QLineEdit()
         self.edit2 = QLineEdit()
         self.edit3 = QLineEdit()
         self.edit4 = QLineEdit()
 
-        # ComboBoxs
         self.box1 = QComboBox()
         self.box1.addItems([
             "linear", "nearest", "zero", "slinear", "quadratic", "cubic",
@@ -51,11 +40,22 @@ class InterpolationComparison(QWidget):
             "previous", "next"
         ])
 
-        # Buttons
         self.btn_start = QPushButton("START!")
-        self.btn_start.clicked.connect(self.btn_start_clicked)
         self.btn_reset = QPushButton("RESET")
-        self.btn_reset.clicked.connect(self.btn_reset_clicked)
+
+        self.start = 0.0
+        self.stop = 0.0
+        self.samples = 0
+        self.function = ""
+        self.first_kind = ""
+        self.second_kind = ""
+
+    def set_interface(self):
+        """This is a function to set interface."""
+
+        # Set window properties
+        self.setWindowTitle("Interpolation Comparison")
+        self.resize(600, 1)
 
         # Labels to layout
         self.layout.addWidget(self.lbl1, 0, 0)
@@ -79,6 +79,10 @@ class InterpolationComparison(QWidget):
         self.layout.addWidget(self.btn_start, 6, 1)
         self.layout.addWidget(self.btn_reset, 6, 0)
 
+        # Set button connections
+        self.btn_start.clicked.connect(self.btn_start_clicked)
+        self.btn_reset.clicked.connect(self.btn_reset_clicked)
+
         # Set layout
         self.setLayout(self.layout)
 
@@ -92,40 +96,42 @@ class InterpolationComparison(QWidget):
                                 QMessageBox.Ok)
             exit(1)
 
-        # Assigning values ​​from fields
+        # Checking and assigning values ​​taken from fields
         try:
-            start = float(self.edit1.text())
-            stop = float(self.edit2.text())
-            samples = int(self.edit3.text())
+            self.start = float(self.edit1.text())
+            self.stop = float(self.edit2.text())
+            self.samples = int(self.edit3.text())
         except (ValueError):
             QMessageBox.warning(self, "Error",
                                 "You have entered an invalid data type!",
                                 QMessageBox.Ok)
             exit(1)
-        function = self.edit4.text()
-        first_kind = self.box1.currentText()
-        second_kind = self.box2.currentText()
 
-        # Checking if the START value is less than the STOP value
-        if (start >= stop):
+        # Assigning values ​​taken from fields
+        self.function = self.edit4.text()
+        self.first_kind = self.box1.currentText()
+        self.second_kind = self.box2.currentText()
+
+        # Checking START and STOP values
+        if (self.start >= self.stop):
             QMessageBox.warning(
                 self, "Error",
                 "The stop value must be greater than the start value",
                 QMessageBox.Ok)
             exit(1)
 
-        # Checking if the number of samples is greater than 5
-        if (samples <= 5):
+        # Checking the number of samples
+        if (self.samples <= 5):
             QMessageBox.warning(
                 self, "Error", "The number of samples must be greater than 5!",
                 QMessageBox.Ok)
             exit(1)
 
         # Run interpolation function
-        interp_obj = interpolation.Interpolation(start, stop, samples,
-                                                 function, first_kind,
-                                                 second_kind)
-        interp_obj.interpolation_graph()
+        interp = interpolation.Interpolation(self.start, self.stop,
+                                             self.samples, self.function,
+                                             self.first_kind, self.second_kind)
+        interp.interpolation_graph()
 
     def btn_reset_clicked(self):
         """This is a function for handling btn_reset clicks."""
@@ -134,11 +140,13 @@ class InterpolationComparison(QWidget):
         self.edit2.clear()
         self.edit3.clear()
         self.edit4.clear()
+
         self.box1.clear()
         self.box1.addItems([
             "linear", "nearest", "zero", "slinear", "quadratic", "cubic",
             "previous", "next"
         ])
+
         self.box2.clear()
         self.box2.addItems([
             "linear", "nearest", "zero", "slinear", "quadratic", "cubic",
@@ -156,8 +164,9 @@ def main():
     """This is a function to run app."""
 
     app = QApplication(sys.argv)
-    window = InterpolationComparison()
-    window.show()
+    gui = ApplicationGUI()
+    gui.set_interface()
+    gui.show()
     sys.exit(app.exec_())
 
 
